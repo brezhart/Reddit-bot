863224f4de972204e58c9f95b0f93c4c64f2261a
brezhart <47702438+brezhart@users.noreply.github.com>
2020-05-04 18:00:40 +0300

    Add files via upload

--- app.js
+++ app.js
@@ -13,7 +13,6 @@ function isSubRedditExist(name, callback) {
     fetch(url, settings)
         .then(res => res.json())
         .then((json) => {
-            console.log("JSON", json);
             if (json.message == "Not Found"){
                 callback("dontExist");
                 return 0;
@@ -38,72 +37,97 @@ function splitError(command,ctx){
 }
 
 
-function checkIfUserExist(id) {
+function checkIfSUserWasCreated(id) {
     try{
-        let subRedditsOfUser =  JSON.parse(fs.readFileSync(`users/${id}.json`)).subreddits;
+        let subRedditsOfUser =  JSON.parse(fs.readFileSync(`users/${id}.json`)).reddits;
         return subRedditsOfUser;
     } catch (e) {
         return false;
     }
 
 }
+function checkIfSubRedditWasCreated(name) {
+    try{
+        let useresOfSubReddit =  JSON.parse(fs.readFileSync(`reddits/${name}.json`)).users;
+        return useresOfSubReddit;
+    } catch (e) {
+        return false;
+    }
 
-
+}
+function addUserTolistOfUsers(user_id) {
+    let listOfUsers = JSON.parse(fs.readFileSync("users.json")).users;
+    listOfUsers.push(user_id);
+    fs.writeFileSync("users.json", JSON.stringify({users: listOfUsers}))
+}
+function addRedditToListOfReddits(redditName) {
+    let listOfReddits = JSON.parse(fs.readFileSync("reddits.json")).reddits;
+    listOfReddits.push(redditName);
+    fs.writeFileSync("reddits.json", JSON.stringify({reddits: listOfReddits}))
+}
 
 function followReddit(redditName,userId, callback){
 
 
-    let reddits = JSON.parse(fs.readFileSync('reddits.json')).reddits;
-    let flag = false;
-    for (let i = 0; i < reddits.length; i++){
-        if (redditName == reddits[i]){
-            console.log("HERACK");
-            let subs = JSON.parse(fs.readFileSync(`reddits/${redditName}.json`)).users;
-            for (let g = 0; g < subs.length; g++){
-                if (subs[g] == userId){
-                    console.log("ALREADY");
-                    callback("already");
-                    return 0;
-                }
-            }
-            console.log("DONT CARE", userId);
 
-            subs.push(userId);
-            fs.writeFileSync(`reddits/${redditName}.json`, JSON.stringify({users:subs}));
-            flag = true;
-            callback('ok');
-            return 0;
-        };
-    }
-    if (!flag){
-        console.log("HERE");
-        console.log(reddits);
-        reddits.push(redditName);
+    let subReddit = checkIfSubRedditWasCreated(redditName);
+    let user = checkIfSUserWasCreated(userId);
 
-        isSubRedditExist(redditName, function(res){
-            if (res == "ok") {
-                fs.writeFileSync(`reddits.json`, JSON.stringify({reddits: reddits}));
+    if (subReddit === false){
+
+        isSubRedditExist(redditName,function(res){
+            if (res === "ok"){
+                addRedditToListOfReddits(redditName);
                 fs.writeFileSync(`reddits/${redditName}.json`, JSON.stringify({users: [userId]}));
+                if (user === false){
+                    fs.writeFileSync(`users/${userId}.json`,       JSON.stringify({reddits: [redditName]}));
+                    addUserTolistOfUsers(userId);
+                } else {
+                    user.push(redditName);
+                    fs.writeFileSync(`users/${userId}.json`,       JSON.stringify({reddits: user}));
+                }
+
             }
             callback(res);
-            return 0;
         });
-    };
+    } else {
+        let res = 'ok';
+        if (user === false){
+            fs.writeFileSync(`users/${userId}.json`, JSON.stringify({reddits: [redditName]}));
+            addUserTolistOfUsers(userId);
+        } else {
+            let flag = false;
+            for (let i = 0; i < user.length; i++ ){
+                if (redditName === user[i]){
+                    res = 'already';
+                    flag = true;
+                    break;
+                }
+            }
+            if (!flag) {
+                user.push(redditName);
+                fs.writeFileSync(`users/${userId}.json`, JSON.stringify({reddits: user}));
+                subReddit.push(userId);
+                fs.writeFileSync(`reddits/${redditName}.json`, JSON.stringify({users: subReddit}));
+            }
+        }
+        callback(res);
+    }
+
 }
 
 
 let checker = 123;
 
 bot.command('подписка', (ctx) =>{
-    console.log("HERE");
     splittedMessage = ctx.message.body.split(" ");
     if (splittedMessage.length < 2){
         splitError('подписка', ctx);
         return 0;
     }
 
-    followReddit(splittedMessage[1].toUpperCase(), ctx.message.user_id, function(res){
-        console.log("1");
+    followReddit(encodeURIComponent(splittedMessage[1].toUpperCase()), ctx.message.user_id, function(res){
+        console.log(res);
         if (res == "ok"){
             ctx.reply(`Вы успешно подписались на ${splittedMessage[1]}`);
         } else if (res == "already"){
@@ -118,147 +142,102 @@ bot.command('подписка', (ctx) =>{
 
 
 });
+bot.command('Мои подписки', (ctx) =>{
+    try {
+        let reddits = JSON.parse(fs.readFileSync(`users/${ctx.message.user_id}.json`)).reddits;
+        let answer = "Ваши подписки: ";
+        for (let i = 0; i < reddits.length; i++){
+            answer+=("\n"+reddits[i]);
+        }
+        ctx.reply(answer);
+
+    } catch (e) {
+        ctx.reply("Вы не подписанны ни на один сабреддит");
+        return 0;
+    };
+
+});
 
 
 
 
 
-// bot.command('отписка', (ctx) =>{
-//     console.log('Отписка');
-//
-//     splittedMessage = ctx.message.body.split(" ");
-//     if (splittedMessage.length < 2){
-//         splitError('отписка', ctx);
-//         return 0;
-//     }
-//
-//     users = JSON.parse(fs.readFileSync('users.json')).users;
-//     flag = false;
-//     ind = 0;
-//     for (let i = 0; i < users.length; i++){
-//         if (users[i] == ctx.message.user_id){
-//             ind = i;
-//             flag = true;
-//         }
-//     }
-//     if (!flag){
-//         ctx.reply('Вы не подписанны');
-//     } else{
-//         ctx.reply("ОТПИСАЛСЯ");
-//         users.splice(ind,1);
-//         fs.writeFileSync('users.json', JSON.stringify({users:users}));
-//     }
-// });
+bot.command('отписка', (ctx) =>{
+    console.log('Отписка');
+
+    splittedMessage = ctx.message.body.split(" ");
+
+    if (splittedMessage.length < 2){
+        splitError('отписка', ctx);
+        return 0;
+    }
+
+    let userId = ctx.message.user_id;
+    let redditName = splittedMessage[1].toUpperCase();
+    let subReddit = checkIfSubRedditWasCreated(redditName);
+    let user = checkIfSUserWasCreated(userId);
+
+
+    if (subReddit === false || user === false){
+        ctx.reply(`Вы не подписанны на  ${redditName}`)
+    } else {
+        let ind;
+        let flag = false;
+        for (let i = 0; i < user.length; i++){
+            if (user[i] == redditName){
+                ind = i;
+                flag = true;
+                break
+            }
+        }
+        if (flag){
+            user.splice(ind,1);
+            for (let i = 0; i < subReddit.length; i++){
+                if (userId == subReddit[i]){
+                    subReddit.splice(i,1);
+                    break;
+                }
+            }
+            if (user.length){
+                fs.writeFileSync(`users/${userId}.json`, JSON.stringify({reddits: user}));
+            } else {
+                fs.unlinkSync(`users/${userId}.json`);
+
+                let listOfUsers = JSON.parse(fs.readFileSync("users.json")).users;
+                for (let i = 0; i < listOfUsers.length;i++){
+                    if (userId==listOfUsers[i]){
+                        listOfUsers.splice(i,1);
+                        fs.writeFileSync("users.json", JSON.stringify({users:listOfUsers}));
+                        break;
+                    }
+                }
+
+            }
+            if (subReddit.length){
+                fs.writeFileSync(`reddits/${redditName}.json`, JSON.stringify({users: subReddit}));
+            } else {
+                fs.unlinkSync(`reddits/${redditName}.json`);
+
+                let listOfReddits = JSON.parse(fs.readFileSync("reddits.json")).reddits;
+                for (let i = 0; i < listOfReddits.length;i++){
+                    if (redditName==listOfReddits[i]){
+                        listOfReddits.splice(i,1);
+                        fs.writeFileSync("reddits.json", JSON.stringify({reddits: listOfReddits}));
+                        break;
+                    }
+                }
+
+            }
+            ctx.reply(`Вы успешно отписались от ${redditName}`)
+        } else {
+            ctx.reply(`Вы не подписанны на  ${redditName}`)
+        }
+    }
+
+});
 
 
 bot.startPolling();
 
 
 
-
-// // Setup basic express server
-// var express = require('express');
-// var app = express();
-// var server = require('http').createServer(app);
-// var io = require('socket.io')(server);
-// var port = process.env.PORT || 80;
-// var request = require("request");-ap
-// var router = express.Router();
-// app.listen(port,function () {
-// });
-//
-// app.use(function(req, res, next) {
-//     res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
-//     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-//     next();
-// });
-//
-// let tokens = {vk:"7e155b83c9afc3c67b06e45b6465246bd69fe9efb7eb898531d1a8e1c9ca4754e1698c29b5ef60bb4a5af"};
-//
-//
-// function sendMessage(receiver, text) {
-//     url = `https://api.vk.com/method/messages.send?user_id=${receiver}&message=${text}&access_token=${tokens.vk}&v=5.103&random_id=${gRI(1,99999999999)}`;
-//     request({url: url, json: true}, function (error, response, body) {
-//         if (!error && response.statusCode === 200) {
-//             console.log("OKAY", response)
-//         } else {
-//             console.log("ERROR")
-//         }
-//     });
-// }
-//
-//
-//
-// // router.get('/', function (req,res,next) {
-// //     console.log(req);
-// //     nJSOS = JSON.parse(req.body);
-// //
-// //     if (req.body.type === "confirmation"){
-// //         res.send('9477a45d');
-// //         return 0;
-// //     }
-// //
-// //     if (nJSOS.type === "message_new"){
-// //         sendMessage(nJSOS.object.user_id,nJSOS.object.text);
-// //     }
-// //     res.send('ok')
-// // });
-//
-// router.post('/',function(req,res,next) {
-//     console.log("POST", req);
-//     if (req.body.type === "confirmation"){
-//         res.send('7f4593fd')
-//     }
-// });
-//
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// // Setup basic express server
-// var express = require('express');
-// var app = express();
-// var server = require('http').createServer(app);
-// var io = require('socket.io')(server);
-// var port = process.env.PORT || 3000;
-// var request = require("request");
-// app.listen(3000,function () {
-// });
-//
-// app.use(function(req, res, next) {
-//     res.header("Access-Control-Allow-Origin", "codepen.io"); // update to match the domain you will make the request from
-//     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-//     next();
-// });
-//
-//
-//
-// var getScoring = function(callback) {
-//     var url = "https://earthmc.net/map/factions/tiles/_markers_/marker_earth.json";
-//     let get;
-//     get = request({url: url, json: true}, function (error, response, body) {
-//         if (!error && response.statusCode === 200) {
-//             callback(body); // Print the json response
-//         }
-//     });
-//     console.log(get);
-// };
-//
-// app.get('/',function(req,res) {
-//
-//     function upd(arg) {
-//         res.json(arg)
-//     }
-//     getScoring(upd);
-// });
