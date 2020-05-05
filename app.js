6fc6fa812add69274b30f9f9a1574515dfa005be
brezhart <47702438+brezhart@users.noreply.github.com>
2020-05-05 10:25:51 +0300

    Add files via upload

--- app.js
+++ app.js
@@ -4,6 +4,17 @@ const VkBot = require('node-vk-bot-api');
 const bot = new VkBot(tokens.vk);
 const fs = require('fs');
 const fetch = require('node-fetch');
+const http = require('http');
+const https = require('https');
+
+
+
+const {Bot} = require('node-vk-bot');
+const imageUploader = new Bot({
+    token: '7e155b83c9afc3c67b06e45b6465246bd69fe9efb7eb898531d1a8e1c9ca4754e1698c29b5ef60bb4a5af',
+    group_id: "194737442"
+}).start();
+
 
 function isSubRedditExist(name, callback) {
 
@@ -157,9 +168,22 @@ bot.command('Мои подписки', (ctx) =>{
     };
 
 });
+// bot.command("Посты", (сtx) =>{
+//     console.log(ctx);
+//     if (ctx.message.user_id == 241007366){
+//         messageSender();
+//     } else
+//         ctx.reply("Вы не админ. Используйте 'Мои посты'")
+// });
 
 
+bot.command('Команды', (ctx) =>{
+    ctx.reply(`Reddit bot AlphaTESTV1.0;\n
 
+Команды: подписка "название", отписка "название", мои подписки, Мои посты.\n
+
+Если вы не знаете на что подписаться, вот список сабреддитов: Anime, EarthPorn, Europe`)
+});
 
 
 bot.command('отписка', (ctx) =>{
@@ -237,7 +261,107 @@ bot.command('отписка', (ctx) =>{
 });
 
 
+function numToOkView(num) {
+    if (num >= 1000){
+        return (num/1000).toFixed(1) + "K";
+    }
+     return num
+}
+
+function fixedFromCharCode (codePt) {
+    if (codePt > 0xFFFF) {
+        codePt -= 0x10000;
+        return String.fromCharCode(0xD800 + (codePt >> 10), 0xDC00 + (codePt & 0x3FF));
+    }
+    else {
+        return String.fromCharCode(codePt);
+    }
+}
+
+
+
+// function messageSender() {
+//     listOfReddits = JSON.parse(fs.readFileSync('reddits.json')).reddits;
+//     function sender(iter) {
+//
+//         let url = `https://www.reddit.com/r/${listOfReddits[iter]}/hot/.json?limit=1`;
+//
+//         let settings = { method: "Get" };
+//         fetch(url, settings)
+//             .then(res => res.json())
+//             .then((json) => {
+//                 let users = JSON.parse(fs.readFileSync(`reddits/${listOfReddits[iter]}.json`)).users;
+//                 let unfoldData = json.data.children[json.data.children.length-1].data;
+//                 let messageToSend = `Из: ${unfoldData.subreddit} \n ${unfoldData.title} \n\u2B06${numToOkView(unfoldData.score)}\n${fixedFromCharCode(0x1F4AC)}${numToOkView(unfoldData.num_comments)}`;
+//                 if (unfoldData.thumbnail == "") {
+//                     bot.sendMessage(users, messageToSend);
+//                     if (iter < listOfReddits.length - 1) {
+//                         sender(iter + 1);
+//                     }
+//                 } else {
+//                     const file = fs.createWriteStream("photoForUpload.jpg");
+//                     console.log(unfoldData.thumbnail);
+//                     const request = https.get(unfoldData.thumbnail, function(response) {
+//                         response.pipe(file);
+//                         let prom = imageUploader.uploadPhoto('photoForUpload.jpg');
+//                         prom.then(function (photo) {
+//                             bot.sendMessage(users, messageToSend , `photo${photo.owner_id}_${photo.id}`);
+//                             if (iter < listOfReddits.length - 1) {
+//                                 sender(iter + 1);
+//                             }
+//                         }, function (error) {
+//                         });
+//                     });
+//                 }
+//             });
+//     }
+//     sender(0)
+// }
+// // bot.sendMessage(241007366, "yl","c67H1USawwc.jpg");
 bot.startPolling();
+bot.command('Мои посты', (ctx) =>{
+   try {
+       let b =  JSON.parse(fs.readFileSync(`users/${ctx.message.user_id}.json`)).reddits;
+       messageSenderToOneUser(ctx.message.user_id)
+   } catch(e) {
+       ctx.reply("Вы не подписанны ни на один сабреддит");
+   }
+});
 
+function messageSenderToOneUser(user_id){
+    listOfReddits = JSON.parse(fs.readFileSync(`users/${user_id}.json`)).reddits;
+    function sender(iter) {
 
+        let url = `https://www.reddit.com/r/${listOfReddits[iter]}/hot/.json?limit=1`;
 
+        let settings = { method: "Get" };
+        fetch(url, settings)
+            .then(res => res.json())
+            .then((json) => {
+                let users = JSON.parse(fs.readFileSync(`reddits/${listOfReddits[iter]}.json`)).users;
+                console.log(json);
+                let unfoldData = json.data.children[json.data.children.length-1].data;
+                let messageToSend = `Из: ${unfoldData.subreddit} \n ${unfoldData.title} \n\u2B06${numToOkView(unfoldData.score)}\n${fixedFromCharCode(0x1F4AC)}${numToOkView(unfoldData.num_comments)}`;
+                if (unfoldData.thumbnail == "") {
+                    bot.sendMessage(user_id, messageToSend);
+                    if (iter < listOfReddits.length - 1) {
+                        sender(iter + 1);
+                    }
+                } else {
+                    const file = fs.createWriteStream("photoForUpload.jpg");
+                    const request = https.get(unfoldData.thumbnail, function(response) {
+                        response.pipe(file);
+                        let prom = imageUploader.uploadPhoto('photoForUpload.jpg');
+                        prom.then(function (photo) {
+                            bot.sendMessage(user_id, messageToSend , `photo${photo.owner_id}_${photo.id}`);
+                            if (iter < listOfReddits.length - 1) {
+                                sender(iter + 1);
+                            }
+                        }, function (error) {
+                        });
+                    });
+                }
+            });
+    }
+    sender(0)
+}
